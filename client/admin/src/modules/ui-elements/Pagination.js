fb6a40f8eaa1b95c5b4ca0b5f5bbfd714b6b70a9
Varvara_Turavets <varvara_turavets@epam.com>
2017-08-09 18:11:39 +0300

    updated pagination

--- client/admin/src/modules/ui-elements/Pagination.js
+++ client/admin/src/modules/ui-elements/Pagination.js
@@ -4,8 +4,7 @@ import classNames from 'classnames'
 const styles = StyleSheet.create({
   pagination: {
     display: 'inline-flex',
-    alignItems: 'center',
-	  backgroundColor: '#f6f6f6'
+    alignItems: 'center'
   },
   wrapper: {
     textAlign: 'center'
@@ -15,19 +14,17 @@ const styles = StyleSheet.create({
     cursor: 'pointer'
   },
   disabledLink: {
-    link: 'redContainer',
-    opacity: 0.6
+    opacity: 0.4
 
   },
-  paginationItems: {
-	  listStyle: 'none',
+  paginationItemContainer: {
+    listStyle: 'none',
     padding: '8px',
   },
-	paginationItem: {
-		borderRadius: '50%',
-    color: 'rgba(33,33,33 ,1)',
+  paginationItem: {
+    borderRadius: '50%',
     display: 'inline-block',
-    marginRight : '5px',
+    marginRight: '5px',
     transition: '.15s ease-in',
     cursor: 'pointer',
     fontSize: '14px',
@@ -35,69 +32,49 @@ const styles = StyleSheet.create({
     lineHeight: '20px',
     minWidth: '35px',
     padding: '8px 0',
-		verticalAlign: 'middle',
+    verticalAlign: 'middle',
     ':hover': {
       background: '#eeeff3',
-	    boxShadow: '0 2px 5px 0 rgba(0, 0, 0, 0.26)'
+      boxShadow: '0 2px 5px 0 rgba(0, 0, 0, 0.26)'
     }
-	},
-	paginationCurrentPage: {
-		background: '#8c95a5',
+  },
+  paginationActiveItem: {
+    background: '#8c95a5',
     color: '#fff',
-		boxShadow: '0 2px 5px 0 rgba(0, 0, 0, 0.26)'
+    boxShadow: '0 2px 5px 0 rgba(0, 0, 0, 0.26)',
+    ':hover': {
+      color: '#fff',
+      background: '#8c95a5',
+    }
   }
 });
 
-const renderPaginationItemsBigData = (pageNumber, pageCount, requestPage) => {
-	const startItem = pageCount > 10 ? pageNumber - 8 : 0;
-	return (
-    <ul className={css(styles.paginationItems)}>
-			{[...Array( 10)].map((x, i) =>
-				i === 0  ?
-          <li key={i + 1}
-              className={classNames({
-						    [css(styles.paginationItem, styles.paginationCurrentPage)]: i === 0
-					    })}
-              onClick={() => requestPage( pageNumber )}
-          >
-						{pageNumber + 1}
-          </li>
-					:
-          <li key={i + 1}
-              className={css(styles.paginationItem)}
-              onClick={() => requestPage(+i + pageNumber)}
-          >
-	          {pageNumber + i + 1}
-          </li>
-			)}
-    </ul>
-	)
-};
-
 const renderPaginationItems = (pageNumber, pageCount, requestPage) => {
-  const startItem = pageCount > 10 ? pageNumber - 8 : 0;
-	return (
-    <ul className={css(styles.paginationItems)}>
-	    {[...Array( pageCount)].map((x, i) =>
-        i == pageNumber ?
-        <li key={i + 1}
-            className={classNames({
-              [css(styles.paginationItem, styles.paginationCurrentPage)]: pageNumber === i
-            })}
-            onClick={() => requestPage(+i)}
-        >
-          {i+1}
-        </li>
-        :
+  const ItemCount = pageCount > 9 ? 9 : pageCount;
+  return (
+    <ul className={css(styles.paginationItemContainer)}>
+      {[...Array(ItemCount)].map((x, i) =>
+        pageCount < 10 || pageNumber < 9 ?
           <li key={i + 1}
-              className={classNames({
-		            [css(styles.paginationItem)]: pageNumber !== i,
-	            })}
+              className={classNames(
+                css(styles.paginationItem),
+                {[css(styles.paginationActiveItem)]: i === pageNumber}
+              )}
               onClick={() => requestPage(+i)}
           >
-		        {i+1}
+            {i + 1}
           </li>
-	    )}
+          :
+          <li key={i + 1}
+              className={classNames(
+                css(styles.paginationItem),
+                {[css(styles.paginationActiveItem)]: i === 8}
+              )}
+              onClick={() => requestPage(pageNumber - 8 + i)}
+          >
+            {pageNumber - 7 + i}
+          </li>
+      )}
     </ul>
   )
 };
@@ -114,12 +91,7 @@ const Pagination = ({pageNumber, requestPage, pageCount}) => {
             <path d="M0-.5h24v24H0z" fill="none"/>
           </svg>
         </div>
-	      {
-		      pageNumber > 10 ?
-	        renderPaginationItems(pageNumber, pageCount, requestPage)
-          :
-		      renderPaginationItemsBigData(pageNumber, pageCount, requestPage)
-	      }
+	      {renderPaginationItems(pageNumber, pageCount, requestPage)}
         <div className={nextArrow} onClick={() => requestPage(+pageNumber + 1)}>
           <svg fill="#000000" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
             <path d="M8.59 16.34l4.58-4.59-4.58-4.59L10 5.75l6 6-6 6z"/>
