94cc2eee4463414d976dba6f774575e2c09fa583
Maksim_Kandybovich <Maksim_Kandybovich@epam.com>
2017-10-23 12:01:20 +0300

    added snapshot testing for admin

--- client/admin/src/modules/ui-elements/PageCaption.test.js
+++ client/admin/src/modules/ui-elements/PageCaption.test.js
@@ -3,6 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import renderer from 'react-test-renderer';
 import PageCaption from './PageCaption';
 
 Enzyme.configure({ adapter: new Adapter() });
@@ -10,6 +11,13 @@ Enzyme.configure({ adapter: new Adapter() });
 it('renders without crashing', () => {
 	const div = document.createElement('div');
 	ReactDOM.render(<PageCaption />, div);
+});
+
+it('renders correctly', () => {
+	const tree = renderer
+		.create(<PageCaption text="Welcome" />)
+		.toJSON();
+	expect(tree).toMatchSnapshot();
 });
 
 test('renders text from props', () => {
