42030ed7c4abe9cbdd3537054ac58295a012b83c
Varvara_Turavets <varvara_turavets@epam.com>
2017-10-31 23:33:40 +0300

    added folder check for files upload

--- server/storage.js
+++ server/storage.js
@@ -1,8 +1,17 @@
 import multer from 'multer';
+import fs from 'fs';
+
+const ensureExists = (folderPath) => {
+	if (!fs.existsSync(folderPath)) {
+		fs.mkdirSync(folderPath);
+	}
+	return folderPath;
+};
+
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, './client/uploads/');
+    cb(null, ensureExists('./client/uploads/'));
   },
   filename: (req, file, cb) => {
     cb(null,  Date.now() + file.originalname);
