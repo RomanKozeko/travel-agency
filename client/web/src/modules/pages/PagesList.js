96eb95cf4014a295e458a510602b39ef5ef98ee9
Maksim Kandybovich <maksim_kandybovich@epam.com>
2018-02-19 22:25:38 +0300

    new build

--- client/web/src/modules/pages/PagesList.js
+++ client/web/src/modules/pages/PagesList.js
@@ -4,7 +4,7 @@ import PrefixLink from '../ui-elements/PrefixLink'
 const PagesList = ({pages}) => (
     <ul>
       {pages.map((page, i) => (
-        <li key={page._id}><PrefixLink to={`/pages/${page.url}`}>{page.content.title}</PrefixLink></li>
+        <li key={page._id}><PrefixLink to={`/pages/${page.url}`}>{page.content ? page.content.title : ''}</PrefixLink></li>
       ))
       }
     </ul>
