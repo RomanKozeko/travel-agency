5ae13c17c9f699e1f19213894aed2367fd50e8b0
Maksim Kandybovich <m.kandybovich@gmail.com>
2018-05-18 20:30:31 +0300

    recycle  bin icon background color changing

--- server/routes/apiRoutes.js
+++ server/routes/apiRoutes.js
@@ -81,69 +81,69 @@ router.delete('/languages', requireAuth, ApiLanguagesCtrl.delete);
 router.get('/pages', ApiPagesCtrl.get);
 router.get('/pages/getByUrl/:url', ApiPagesCtrl.getOneByUrl);
 router.get('/pages/:id', ApiPagesCtrl.getOne);
-router.put('/pages/:id', requireSignIn, ApiPagesCtrl.put);
-router.delete('/pages', requireSignIn, ApiPagesCtrl.delete);
-router.post('/pages', requireSignIn, ApiPagesCtrl.post);
+router.put('/pages/:id', requireAuth, ApiPagesCtrl.put);
+router.delete('/pages', requireAuth, ApiPagesCtrl.delete);
+router.post('/pages', requireAuth, ApiPagesCtrl.post);
 
 router.get('/media', ApiPhotosCtrl.get);
 router.get('/media/:id', ApiPhotosCtrl.getOne);
-router.put('/media/:id', requireSignIn, ApiPhotosCtrl.put);
-router.post('/media', requireSignIn, upload.single('file'), ApiPhotosCtrl.post);
-router.post('/media-upload', requireSignIn, upload.single('file'), ApiPhotosCtrl.upload);
-router.delete('/media', requireSignIn, ApiPhotosCtrl.delete);
+router.put('/media/:id', requireAuth, ApiPhotosCtrl.put);
+router.post('/media', requireAuth, upload.single('file'), ApiPhotosCtrl.post);
+router.post('/media-upload', requireAuth, upload.single('file'), ApiPhotosCtrl.upload);
+router.delete('/media', requireAuth, ApiPhotosCtrl.delete);
 
 router.get('/hotels', ApiHotelsCtrl.get);
 router.get('/hotels/:id', ApiHotelsCtrl.getOne);
 router.get('/hotelGetByUrl/:url', ApiHotelsCtrl.getOneByUrl);
-router.put('/hotels/:id', requireSignIn, ApiHotelsCtrl.put);
-router.post('/hotels', requireSignIn, ApiHotelsCtrl.post);
-router.delete('/hotels', requireSignIn, ApiHotelsCtrl.delete);
+router.put('/hotels/:id', requireAuth, ApiHotelsCtrl.put);
+router.post('/hotels', requireAuth, ApiHotelsCtrl.post);
+router.delete('/hotels', requireAuth, ApiHotelsCtrl.delete);
 
 router.get('/showPlaces', ApiShowPlacesCtrl.get);
 router.get('/showPlaces/:id', ApiShowPlacesCtrl.getOne);
 router.get('/showPlacesGetByUrl/:url', ApiShowPlacesCtrl.getOneByUrl);
-router.put('/showPlaces/:id', requireSignIn, ApiShowPlacesCtrl.put);
-router.post('/showPlaces', requireSignIn, ApiShowPlacesCtrl.post);
-router.delete('/showPlaces', requireSignIn, ApiShowPlacesCtrl.delete);
+router.put('/showPlaces/:id', requireAuth, ApiShowPlacesCtrl.put);
+router.post('/showPlaces', requireAuth, ApiShowPlacesCtrl.post);
+router.delete('/showPlaces', requireAuth, ApiShowPlacesCtrl.delete);
 
 router.get('/food', ApiFoodCtrl.get);
 router.get('/food/:id', ApiFoodCtrl.getOne);
-router.put('/food/:id', requireSignIn, ApiFoodCtrl.put);
-router.post('/food', requireSignIn, ApiFoodCtrl.post);
-router.delete('/food', requireSignIn, ApiFoodCtrl.delete);
+router.put('/food/:id', requireAuth, ApiFoodCtrl.put);
+router.post('/food', requireAuth, ApiFoodCtrl.post);
+router.delete('/food', requireAuth, ApiFoodCtrl.delete);
 
 router.get('/featured', FeaturedCtrl.get);
 router.get('/featured/:id', FeaturedCtrl.getOne);
-router.put('/featured/:id', requireSignIn, FeaturedCtrl.put);
-router.post('/featured', requireSignIn, FeaturedCtrl.post);
-router.delete('/featured', requireSignIn, FeaturedCtrl.delete);
+router.put('/featured/:id', requireAuth, FeaturedCtrl.put);
+router.post('/featured', requireAuth, FeaturedCtrl.post);
+router.delete('/featured', requireAuth, FeaturedCtrl.delete);
 
 router.get('/news', NewsCtrl.get);
 router.get('/news/:id', NewsCtrl.getOne);
-router.put('/news/:id', requireSignIn, NewsCtrl.put);
-router.post('/news', requireSignIn, NewsCtrl.post);
-router.delete('/news', requireSignIn, NewsCtrl.delete);
+router.put('/news/:id', requireAuth, NewsCtrl.put);
+router.post('/news', requireAuth, NewsCtrl.post);
+router.delete('/news', requireAuth, NewsCtrl.delete);
 
 router.get('/menu', MenuCtrl.get);
-router.put('/menu', requireSignIn, MenuCtrl.put);
-router.post('/menu', requireSignIn, MenuCtrl.post);
-router.delete('/menu', requireSignIn, MenuCtrl.delete);
+router.put('/menu', requireAuth, MenuCtrl.put);
+router.post('/menu', requireAuth, MenuCtrl.post);
+router.delete('/menu', requireAuth, MenuCtrl.delete);
 
 router.get('/contacts', ContactsCtrl.get);
-router.put('/contacts/:id', requireSignIn, ContactsCtrl.put);
-router.post('/contacts', requireSignIn, ContactsCtrl.post);
-router.delete('/contacts', requireSignIn, ContactsCtrl.delete);
+router.put('/contacts/:id', requireAuth, ContactsCtrl.put);
+router.post('/contacts', requireAuth, ContactsCtrl.post);
+router.delete('/contacts', requireAuth, ContactsCtrl.delete);
 
 router.get('/settings', SettingsCtrl.get);
-router.put('/settings/:id', requireSignIn, SettingsCtrl.put);
-router.post('/settings', requireSignIn, SettingsCtrl.post);
-router.delete('/settings', requireSignIn, SettingsCtrl.delete);
+router.put('/settings/:id', requireAuth, SettingsCtrl.put);
+router.post('/settings', requireAuth, SettingsCtrl.post);
+router.delete('/settings', requireAuth, SettingsCtrl.delete);
 
 router.post('/send-email', EmailerCtrl.post);
 
 router.get('/slider', SliderCtrl.get);
-router.put('/slider/:id', requireSignIn, SliderCtrl.put);
-router.post('/slider', requireSignIn, SliderCtrl.post);
-router.delete('/slider', requireSignIn, SliderCtrl.delete);
+router.put('/slider/:id', requireAuth, SliderCtrl.put);
+router.post('/slider', requireAuth, SliderCtrl.post);
+router.delete('/slider', requireAuth, SliderCtrl.delete);
 
 module.exports = router;
